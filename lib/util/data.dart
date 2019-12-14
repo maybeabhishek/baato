017f12efaf487431c2d0a6af29c3f9fb26626f07
abhishek <abhishek.satapathy01@gmail.com>
2019-12-14 22:26:03 +0530

    uplozad

---
+++ lib/util/data.dart
@@ -0,0 +1,47 @@
+List furnitures = [
+  {
+    "name": "Tractors",
+    "img": "assets/1.jpeg",
+  },
+  {
+    "name": "Mini Power Tiller",
+    "img": "assets/4.jpeg",
+  },
+  {
+    "name": "Center Pivot Irrigation",
+    "img": "assets/3.jpeg",
+  },
+  {
+    "name": "Grain Dryer",
+    "img": "assets/2.jpeg",
+  },
+ {
+    "name": "Grain Dryer",
+    "img": "assets/2.jpeg",
+  },
+   {
+    "name": "Grain Dryer",
+    "img": "assets/2.jpeg",
+  },
+   {
+    "name": "Grain Dryer",
+    "img": "assets/2.jpeg",
+  },
+   {
+    "name": "Tractors",
+    "img": "assets/1.jpeg",
+  },
+   {
+    "name": "Tractors",
+    "img": "assets/1.jpeg",
+  },
+   {
+    "name": "Tractors",
+    "img": "assets/1.jpeg",
+  },
+   {
+    "name": "Tractors",
+    "img": "assets/1.jpeg",
+  },
+
+];
